728891722a163d2331859af42126929258b50898
zacharyenright <zachary.enright1@gmail.com>
2024-11-06 14:39:09 -0500

    seeded database

--- config/dbConfig.py
+++ config/dbConfig.py
@@ -1,5 +1,5 @@
 import sqlite3
 def get_db_connection():
-    conn = sqlite3.connect('.db/database.db') 
+    conn = sqlite3.connect('db/database.db') 
     conn.row_factory = sqlite3.Row
     return conn
